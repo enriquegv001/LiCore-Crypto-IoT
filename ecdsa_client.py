086d048bc09c6f26eedbcb90842f7a9f635c80ff
Enrique García Varela <86385297+enriquegv001@users.noreply.github.com>
2023-06-12 01:27:40 -0600

    Conexión RDS, ECDSA, TLS

--- ecdsa_client.py
+++ ecdsa_client.py
@@ -30,7 +30,8 @@ def alg_euc_ext(a,b): # obtener inverso de a, del grupo b
   d = a; x = x2; y = y2 # y es el inverso 
   return d,x,y
     
-def firmado_dsa(curve, m):
+def firmado_dsa(m):
+  curve = registry.get_curve('brainpoolP256r1')
   # Generación de clave
   q = curve.field.n
   P = curve.g # element G of prime order q is chosen in E(GF(p))
@@ -48,7 +49,7 @@ def firmado_dsa(curve, m):
     r = kP.x % q 
     l = alg_euc_ext(q, k)[2] # inverso de k
     s = (l*(h + x*r))%q 
-  return  r, s, Q, m
+  return  curve, r, s, Q, m, 
     
 #Firmado y lista con la curva, r, s y Q
 def firmado_df(df):
@@ -56,7 +57,7 @@ def firmado_df(df):
   for i in df.index: #firmado para cada registro
     curve = registry.get_curve('brainpoolP256r1')
     m = pickle.dumps(df.iloc[i].to_numpy(), protocol=4)
-    r,s,Q,m = firmado_dsa(curve, m)
+    _,r,s,Q,m = firmado_dsa(m)
     a.append([curve, r,s,Q,m])
   return a
 
@@ -85,73 +86,89 @@ def ver_df(a):
   for i in range(len(a)):
     return verificado_dsa(a[i][0],a[i][1],a[i][2],a[i][3],a[i][4])
 
-def info_exchange(client_socket):
+def info_exchange(client_socket, dataframe):
 #-----------------------------Envio de mensajes--------------------
   i = 0
   while i<1:
       decision = input('\nActions:\n-Send (F) \n-Receive (V) \n-End \nSelect your action:\n') #Preguntar
-      message = pickle.dumps(decision, protocol=4) 
-      client_socket.send(message)  # send message 
+      #message = pickle.dumps(decision, protocol=4) 
+      client_socket.send(decision.encode())  # send message 
 
       if decision == 'F': # cliente como firmado
-          r = firmado_df(m) 
-          message = pickle.dumps(r, protocol=4) 
+          message = pickle.dumps(firmado_df(dataframe) , protocol=4) 
+          #message = firmado_df(m).encode(), 
+          print(len(message))
           client_socket.send(message)  # enviar firmado
           ver = client_socket.recv(65507).decode() # recibir verificacion
           print('Cliente: firmado')
           print('Servidor verificación: ' + str(ver))  # show in terminal
           
       elif decision == 'V': # cliente como verificador
-          data = pickle.loads(client_socket.recv(65507), encoding='bytes') # recibir parámetros firmado
-          print('Recived message:')
-          print(pickle.loads(data[0][4], encoding = 'bytes'), '\n')
-          client_socket.send(ver_df(data).encode())  # enviar verificación
-          print('Cliente: verificador',ver_df(data) )
+          #envio query
+          query = pickle.dumps(input('\nInsert sql query:\n'), protocol=4)
+          message = pickle.dumps(firmado_dsa(query), protocol=4)
+          client_socket.send(message)
+
+          #recibir datos
+          curve, r, s, Q, m  = pickle.loads(client_socket.recv(65507), encoding='bytes')
+          print('message recived...')
+          print('\n', verificado_dsa(curve, r, s, Q, m))
+          m = pickle.loads(m, encoding='bytes' )
+
+          for row in m:
+            for col in row:
+                print(col,end=' ')
+            print()
+
+
+
+      #elif decision == 'T':
+      #   client_socket.send('prueba'.encode())
+    
 
       else: #terminar iteraciones
         i = i + 1
 
 #============================Conexión SSL/TLS=========================
-def client_program(m, curve = None): #conexión sin TLS
-  #host = socket.gethostname()#'131.178.102.128' #'44.203.90.217'  # as both code is running on same pc
-  host = "ec2-3-91-204-15.compute-1.amazonaws.com"
-  port = 1234  # socket server port number
-
+def client_program(m, host, port): #conexión sin TLS
   client_socket = socket.socket()  # instantiate
   print('Socket creado')
   client_socket.connect((host, port))  # connect to the server
   print('Socket conectado')
-  info_exchange(client_socket)
+  info_exchange(client_socket, m)
   client_socket.close()  # close the connection
 
 
-def client_tls(): 
+def client_tls(m, hostname, port): 
   #hostname = "ec2-44-204-5-126.compute-1.amazonaws.com"
   #hostname = "ec2-35-175-217-227.compute-1.amazonaws.com"
-  hostname = "ec2-3-91-204-15.compute-1.amazonaws.com"
+  #hostname = "ec2-3-91-204-15.compute-1.amazonaws.com"
   
 
   context = ssl.create_default_context()
-  #context.load_verify_locations('C:\\Users\\Enrique\\Downloads\\ec-cacert.pem')
-  context.load_verify_locations('C:\\Users\\Enrique\\Downloads\\Cripto Reto\\Client\\new\\ec-cacert-inst4.pem')
+  #context.loads_verify_locations('C:\\Users\\Enrique\\Downloads\\ec-cacert.pem')
+  #context.loads_verify_locations('C:\\Users\\Enrique\\Downloads\\Cripto Reto\\Client\\new\\ec-cacert-inst4.pem')
+  context.load_verify_locations(r"C:\Users\Enrique\OneDrive - Instituto Tecnologico y de Estudios Superiores de Monterrey\Documents\Escuela\FJ23\Algebra moderna Cripto\reto\Reto-Git\LiCore-Crypto-IoT\ec-cacert-inst4.pem")
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   print('socket creado')
   ssock = context.wrap_socket(sock, server_hostname=hostname)
   #ssock = ssl.wrap_socket(sock, certfile = 'ec-cacert.pem')
   print('socket warpped')
-  ssock.connect((hostname, 1234))
+  ssock.connect((hostname, port))
   print('Socket version: ', ssock.version())
-  #ssock.send("test".encode())
-  #print(ssock.recv(1000).decode())
-  #ssock.send()
-  info_exchange(ssock)
-  #it will close by server
+
+  info_exchange(ssock, m)
 
 
 if __name__ == '__main__':
     #base de datos
     dataset = pd.read_csv(r'Prosumer_ABC.csv', header = 0, sep = ";")
     #curve = registry.get_curve('brainpoolP256r1')
-    m = dataset.iloc[0:2]
-    #client_program(m)
-    client_tls()
+    dataframe = dataset.iloc[0:5]
+
+    host_public = socket.gethostname()#'201.130.58.26' #mine
+    #host = "ec2-3-91-204-15.compute-1.amazonaws.com" #aws
+    port = 1234
+
+    client_program(dataframe, host_public, port)
+    #client_tls()
